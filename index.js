591920dee17b59cef9fc348925583f24165173eb
mbron770 <mbronfin.tsd@gmail.com>
2023-06-04 15:22:59 -0400

    write functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (arr) => arr.slice(0,2)
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = (arr) => arr.slice(2,4)
+
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    return function(fare){
+        return fare * number
+    }
+
+    // return fareDoubler(fare)
+}
+
+const fareDoubler = (fare) => fare*2
+
+const fareTripler = (fare) => fare*3
+
+const selectDifferentDrivers = (driversArray, fx1) => fx1(driversArray)
+  
